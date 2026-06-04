95987d3ca02279a53a647f5eb5c290a5ba5dd94f
Timothy Poisal <48577624+tjpoisal@users.noreply.github.com>
2026-06-04 13:58:55 -0400

    fix: exclude .eslintrc.js from ESLint to prevent Babel parser error
    The @react-native ESLint config uses @babel/eslint-parser which requires a
    Babel config file. Excluding .eslintrc.js from linting avoids the parse error.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
     'prettier/prettier': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
