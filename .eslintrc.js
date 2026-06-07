aabd00ca129685955a4ee92549dc8caef3b571c6
Timothy Poisal <48577624+tjpoisal@users.noreply.github.com>
2026-06-07 13:23:35 -0400

    fix: apply add-package-lock-file branch changes - update deps, fix eslint config, add package-lock stub, improve CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
   root: true,
   extends: '@react-native',
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    'prettier/prettier': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
-  },
 };
