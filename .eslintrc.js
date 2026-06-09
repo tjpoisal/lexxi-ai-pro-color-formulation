e4aa04307159eddb6ac2edbe42032b56f1fc7768
Timothy Poisal <48577624+tjpoisal@users.noreply.github.com>
2026-06-08 22:21:42 -0400

    fix: add requireConfigFile: false to allow ESLint to parse index.js without Babel config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   root: true,
   extends: '@react-native',
   ignorePatterns: ['.eslintrc.js'],
+  parserOptions: {
+    requireConfigFile: false,
+  },
   rules: {
     'prettier/prettier': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
